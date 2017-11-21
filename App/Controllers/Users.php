65f05982d36d0efeb4a7525bdce965571ea24679
caoym <caoyangmin@gmail.com>
2017-11-21 17:13:01 +0800

    make BasicAuth work

--- App/Controllers/Users.php
+++ App/Controllers/Users.php
@@ -11,6 +11,7 @@ class Users
     /**
      * 通过实现BasicAuth 演示 hook 的使用
      * 此接口在浏览器访问, 将弹出密码输入框, 用户名密码在 config.php 中配置
+     * @route GET /myself
      * @hook \App\Hooks\BasicAuth
      */
     public function getCurrentUser(){
